68eea2edb95cc8ec95949da9f37238abcbcb5073
vyshuks <vyshuks@gmail.com>
2020-02-28 02:16:15 +0530

    sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,17 +1,64 @@
-def print_board(bo):
-    for i in range(len(bo)):
+def print_board(board):
+    for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
 
-        for j in range(len(bo[0])):
+        for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
             if j == 8:
-                print(bo[i][j])
+                print(board[i][j])
             else:
-                print(str(bo[i][j]) + " ", end="")
+                print(str(board[i][j]) + " ", end="")
 
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 0:
+                return (i,j)
+    return None
+
+def is_valid(board, num, pos):
+    # for row
+    for i in range(len(board)):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+    
+    # for column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # for box
+    box_x = (pos[1] // 3) * 3
+    box_y = (pos[0] // 3) * 3
+
+    for i in range(box_y, box_y+3):
+        for j in range(box_x, box_x+3):
+            if board[i][j] == num and pos != (i,j):
+                return False
+    
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
 
 def main():
     board = [
@@ -26,6 +73,12 @@ def main():
         [0, 4, 9, 2, 0, 6, 0, 0, 7],
     ]
     print("Before Solving")
+    print("==============")
+    print_board(board)
+
+    solve(board)
+
+    print("After Solving")
     print("==============")
     print_board(board)
 
